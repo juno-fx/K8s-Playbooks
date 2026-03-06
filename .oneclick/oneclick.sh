c6db6ebfa74edfe2276689643d2d48966b7caac7
ptownley <ptownley@juno-innovations.com>
2026-03-06 11:25:43 +0000

    Pass values from bootstrap to ansible

--- .oneclick/oneclick.sh
+++ .oneclick/oneclick.sh
@@ -8,4 +8,11 @@ if [ ! -f /tmp/juno_bootstrap_chart_values.yaml ]; then
   exit 1
 fi
 
-ansible-playbook -i /oneclick/inventory /oneclick/oneclick-playbook.yaml
+extra_vars=${1}
+extra_vars_full_args=""
+if [ "${extra_vars}" ]; then
+    extra_vars_full_args=--extra-vars "$extra_vars"
+fi
+
+
+echo ansible-playbook -i /oneclick/inventory /oneclick/oneclick-playbook.yaml "$extra_vars_full_args"
